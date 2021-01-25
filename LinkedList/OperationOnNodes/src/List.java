3be5b2af771b34733bf6c65327b06f423444b767
sethgreen23 <dridi.chaith@gmail.com>
2021-01-25 20:22:19 +0100

    sublist function

--- LinkedList/OperationOnNodes/src/List.java
+++ LinkedList/OperationOnNodes/src/List.java
@@ -1,24 +1,68 @@
+import java.util.Arrays;
 
 public class List {
 	
 	public static void main(String[] args) {
 		Node start=null ;
 		start = createList(start,new int[]{25,19,6,3,12,35,4});
-		//toString(start);
-		//System.out.println(size(start));
-		//System.out.println(sum(start));
-		//removeLast(start);
-		//toString(start);
-		Node init = copy(start);
+//		toString(start);
+		/*size function*/
+//		System.out.println(size(start));
+		/*sum function*/
+//		System.out.println(sum(start));
+		/*removeLast function*/
+//		removeLast(start);
+//		toString(start);
+		/*copy function*/
+//		Node init = copy(start);
+//		toString(start);
+//		System.out.println("remove last element from the start list");
+//		removeLast(start);
+//		removeLast(start);
+//		toString(start);
+//		System.out.println("Print out the elements of the init list");
+//		toString(init);
+		/*size function*/
 		toString(start);
-		System.out.println("remove last element from the start list");
-		removeLast(start);
-		removeLast(start);
-		toString(start);
-		System.out.println("Print out the elements of the init list");
+		Node init = sublist(start,1,5);
 		toString(init);
+		removeLast(init);
+		toString(start);
+		toString(init);
+		
 	}
 	
+	
+//	Write and test this method:
+//		Node sublist(Node list, int p, int q)
+//		// returns: a new list that contains copies of the q-p nodes of the
+//	// specified list, starting with node number p (starting with 0);
+//	For example, if list is {22, 33, 44, 55, 66, 77, 88, 99}, then sublist(list, 2, 7) will
+//	return the new list {44, 55, 66, 77, 88}. Note that the two lists must be completely independent of each other. Changing one list should have no effect upon the other
+	
+	public static Node sublist(Node start, int p, int q) {
+		int[] input = getElements(start,p,q);
+		Node init=null;
+		init=createList(init,input);
+		return init;
+	}
+	
+	private static int[] getElements(Node start, int p, int q) {
+		int[] input = new int[q-p];
+		int i=0,j=0;
+		Node init = start;
+		while(init.next!=null) {
+			if(i>=p && i<q) {
+				input[j++]=init.data;
+			}
+			i++;
+			init = init.next;
+		}
+		return input;
+	}	
+
+
+	//copy function
 	public static Node copy(Node start) {
 		//make sure the list doesnt contain 1 or 0 elements
 		if(start==null || start.next==null) {
