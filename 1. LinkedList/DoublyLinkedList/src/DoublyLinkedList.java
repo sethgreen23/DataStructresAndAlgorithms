415e630ec7cd3a7f8166264b96c145c683d5d488
sethgreen23 <dridi.chaith@gmail.com>
2021-02-03 12:29:22 +0100

    add the delete in front and at the end method

--- 1. LinkedList/DoublyLinkedList/src/DoublyLinkedList.java
+++ 1. LinkedList/DoublyLinkedList/src/DoublyLinkedList.java
@@ -19,30 +19,77 @@ public class DoublyLinkedList {
 		l.insertInFront(e3);
 		l.insertAtEnd(e4);
 		l.insertInFront(e5);
-		
+		System.out.println("Print From End: ");
 		l.printFromEnd();
+		System.out.println("Print From Front: ");
 		l.printFromFront();
 		System.out.println("Head : "+ l.head);
 		System.out.println("Tail : "+l.tail);
+		System.out.println("Delete From Front: ");
+		EmployeeNode removed=l.deleteInFront();
+		System.out.println("Removed Item From Front: "+removed);
+		l.printFromFront();
+		System.out.println("Delete From End: ");
+		removed=l.deleteFromEnd();
+		System.out.println("Removed Item From End: "+removed);
+		l.printFromFront();
+		
 	}
-
+	
+	//delete From end
+	EmployeeNode deleteFromEnd() {
+		//assign tail to the node we want to remove
+		EmployeeNode removedNode=tail;
+		//set the next node of the previous node of tail to the next node of tail
+		tail.getPrev().setNext(tail.getNext());
+		//the previous node to tail become tail
+		tail=tail.getPrev();
+		//we reset the next and previous fields of the removed node to null 
+		//it will be more easier to the garbage collector to get rid of
+		removedNode.setNext(null);
+		removedNode.setPrev(null);
+		return removedNode;
+	}
+	
+	//delete in front
+	//same midset of the method deleteFromEnd
+	EmployeeNode deleteInFront() {
+		EmployeeNode removedNode=head;
+		head.getNext().setPrev(head.getPrev());
+		head=head.getNext();
+		removedNode.setNext(null);
+		removedNode.setPrev(null);
+		return removedNode;
+	}
+	
+	
 	//insert in front
 	void insertInFront(Employee e) {
+		//if the head is not null
 		if(head!=null) {
+			//create node to hold the employee object
 			EmployeeNode node = new EmployeeNode(e);
+			//set the next field of the node to the head node
 			node.setNext(head);
+			//set the previous node of head to the node we want to insert
 			head.setPrev(node);
+			//if the next node of head is null then we assign the tail to it
 			if(head.getNext()==null)
 				head.setNext(tail);
+			//the inserted node become the head
 			head=node;
-		}else {
+		}else {//if the head is null
+			//create a node to hold the employee node
 			EmployeeNode node = new EmployeeNode(e);
+			//the new inserted node become the head
 			head=node;
+			//set the next node of the head to tail
 			head.setNext(tail);
 		}
 	}
 	
 	//insert at end
+	//same mindset of the insertInFront
 	void insertAtEnd(Employee e) {
 		if(tail!=null) {
 			EmployeeNode node = new EmployeeNode(e);
