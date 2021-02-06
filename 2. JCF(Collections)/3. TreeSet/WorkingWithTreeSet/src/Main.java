0b30b0c3166c754052b3f86dc78701ad96d19839
sethgreen23 <dridi.chaith@gmail.com>
2021-02-06 19:16:00 +0100

    commeting treeset

--- 2. JCF(Collections)/3. TreeSet/WorkingWithTreeSet/src/Main.java
+++ 2. JCF(Collections)/3. TreeSet/WorkingWithTreeSet/src/Main.java
@@ -27,6 +27,15 @@ public class Main {
 	}
 
 }
+//output
+/*
+	 using iterator to display TreeSet
+	CA, USA, GB, CHN, JP, GER, FR, RUS, HU
+	[CA, CHN, FR, GB, GER, HU, JP, RUS, USA]
+	[CA, CHN, FR]
+	[GB, GER, HU]
+	[JP, RUS, USA]
+ */
 
 /*
  In Java, there are two ways for objects to be compared: either by means of their natural ordering or by the application of an external Comparator object. Classes whose objects enjoy a
@@ -38,4 +47,14 @@ how its two arguments are ordered: compare(x,y) > 0 means that x is greater than
 compare(x,y) < 0 means that x is less than y, and compare(x,y) == 0 means that x equals y.
 A Comparator object can be passed to a SortedSet constructor to specify how the elements
 should be ordered.
+ */
+
+/*
+ The intrinsic difference between the HashSet and TreeSet classes is their backing data structure. The HashSet class uses a hash table (outlined in Chapter 8), which uses each element’s
+hashCode() to compute its location in the set. The TreeSet class uses a balanced binary search
+tree (outlined in Chapter 12) to store its elements. The advantages and disadvantages of these
+two data structures are summarized in Table 4.2.
+These relative advantages and disadvantages make the choice easy: If you want to preserve the
+natural order of the elements in your set, use the TreeSet class; otherwise, use the HashSet class
+ */
  */
