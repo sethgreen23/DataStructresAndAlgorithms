d090b22705454c06e818baaad030a71b273427a0
sethgreen23 <dridi.chaith@gmail.com>
2021-02-07 13:43:29 +0100

    workign with linkedlist class

---
+++ 2. JCF(Collections)/9. LinkedList/WorkingWithLinkedList/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+/*
+	The LinkedList class uses a doubly linked list to implement the List interface. In addition
+	to the 25 methods specified by the Collection and List interfaces, this class also implements
+	these 11 other methods:
+	void addFirst(E o)
+	void addLast(E o)
+	E element()
+	E getFirst()
+	E getLast()
+	boolean offer(E o)
+	E peek()
+	E poll()
+	E remove()
+	E removeFirst()
+	E removeLast()
+	The add, get, and remove methods that refer to first and last access the first element and
+	last element of the list, respectively. For example, the call
+	list.addFirst(x);
+	would add the object x to the front of the list, making it the first element.
+	Note that the three remove methods listed here are all obviated by the remove(int) method
+	specified by the List interface. The LinkedList methods remove() and removeFirst() are
+	the same as the List method remove(0).
+	The other five new methods (element(), offer(), peek(), poll(), and remove()) are
+	outlined below.
+*/
+}
