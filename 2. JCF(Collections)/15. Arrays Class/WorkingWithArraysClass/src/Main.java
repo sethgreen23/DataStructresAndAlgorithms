f66a6f6b8a7df849505adc95a706b32b4a6b53b3
sethgreen23 <dridi.chaith@gmail.com>
2021-02-08 07:21:29 +0100

    Working with Arrays Class

---
+++ 2. JCF(Collections)/15. Arrays Class/WorkingWithArraysClass/src/Main.java
@@ -0,0 +1,48 @@
+
+public class Main {
+/*
+	The java.util.Arrays class contains static methods for processing arrays, including
+	methods for converting arrays to collections and vice versa. The class is briefly outlined on
+	page 29.
+	Most of the methods in the Arrays class are overloaded with separate versions for the generic
+	array type T[] and for each of the nine specific array types: boolean[], byte[], char[],
+	double[], float[], int[], long[], Object[], and short[]. In addition, most of these have a
+	separate version for a subarray. For example, here are the 18 overloaded versions of the sort()
+	method:
+	void sort(boolean[] a)
+	void sort(boolean[] a, int fromIndex, int toIndex)
+	void sort(byte[] a)
+	void sort(byte[] a, int fromIndex, int toIndex)
+	void sort(c[] a)
+	void sort(byte[] a, int fromIndex, int toIndex)
+	void sort(double[] a)
+	void sort(double[] a, int fromIndex, int toIndex)
+	void sort(int[] a)
+	void sort(int[] a, int fromIndex, int toIndex)
+	void sort(float[] a)
+	void sort(float[] a, int fromIndex, int toIndex)
+	void sort(long[] a)
+	void sort(long[] a, int fromIndex, int toIndex)
+	void sort(Object[] a)
+	void sort(Object[] a, int fromIndex, int toIndex)
+	void sort(short[] a)
+	void sort(short[] a, int fromIndex, int toIndex)
+	<T> void sort(T[] a, Comparator<? super T> c)
+	<T> void sort(T[] a, int fromIndex, int toIndex,
+	Comparator<? super T> c)
+	(There is no sort() method for boolean arrays.)
+	The eight different method() categories are:
+	binarySearch()
+	copyOf()
+	copyOfRange()
+	equals()
+	fill()
+	hashCode()
+	sort()
+	toString()
+	In addition, there is a single asList() method that returns an ArrayList collection containing
+	the elements passed to it, like this:
+	List<String> list = Arrays.asList("CA", "US", "MX");
+	
+*/
+}
