92a23248287ddd426ff29cc7dec474078f89ef4a
sethgreen23 <dridi.chaith@gmail.com>
2021-02-12 23:19:41 +0100

    RPN Calculator adding comment

--- 3. Stack/RPN Calculator/src/Main.java
+++ 3. Stack/RPN Calculator/src/Main.java
@@ -51,3 +51,35 @@ public class Main {
 	}
 
 }
+//output:
+//5
+//9
+//+
+//9.0+5.0 = 14.0
+//3
+///
+//3.0/14.0 = 0.21428571428571427
+//q
+//Thank you for Using our Application.See you next time ;)
+
+/*
+Although the stack data structure is one of the simplest, it is essential in certain important
+applications. Some of these are illustrated in the following examples.
+An arithmetic expression is said to be in postfix notation (also called reverse Polish notation,
+or RPN) if each operator is placed after its operands. For example, the postfix expression for
+3*(4 + 5) is 3 4 5 + *. [The expression 3*(4 + 5) is called an infix expression.] Postfix
+expressions are easier to process by machine than are infix expressions. Calculators that process
+postfix expressions are called RPN calculators.
+
+ */
+
+/*
+ At line 3, the program instantiates a stack of strings, like the one in Example 5.1 on page 103. Then it
+goes into an infinite while loop at line 5, interactively reading a string on each iteration at line 6.
+If the user inputs anything other than +, -, *, /, q, or Q, the program assumes it is a number and pushes
+it onto the stack at line 23. If it is one of the four arithmetic operations, then the top two numbers are
+popped off the stack at lines 9–10 and the operation is applied to them. The result is printed at line 18 and
+then pushed onto the stack at line 19.
+The program ends when the user enters q or Q (for “quit”).
+Human readers tend to prefer infix to postfix notation for arithmetic expressions. The following example converts a given infix expression to postfix.
+ */
