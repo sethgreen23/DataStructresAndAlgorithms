102e439f02598a4f889fc50893fffd3453c4dd07
sethgreen23 <dridi.chaith@gmail.com>
2021-03-05 17:41:08 +0100

    adding comment to the concordance application

--- 6.Hash Table/4.Application/src/Concordance.java
+++ 6.Hash Table/4.Application/src/Concordance.java
@@ -1,12 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Concordance {
+	/*
+	 The TreeMap class extends the AbstractMap class and implements the SortedMap interface.
+	 It is called a tree map because its backing structure is a binary search tree instead of a hash table
+	 But it is still a map with key/value entries. As a binary search tree structure, it sacrifices its O(1)
+	access time, but its keys are ordered.
+	 */
 	private  Map<String,String> map = new HashMap<String,String>();
 	
 	public Concordance(String file) {
@@ -80,3 +85,99 @@ public class Concordance {
 	}
 
 }
+//OUTPUT using TreeMap alphabetically ordered output
+/*
+A=6, 23
+AFTER=3
+ALL=10, 10, 22, 29
+AM=28
+AMBITION=19, 24
+AMBITIOUS=5, 13, 17, 20, 25
+AN=9, 14, 26
+AND=7, 8, 12, 14, 21, 21, 26, 32
+ANSWER=7
+ARE=10
+ART=31
+BE=4, 19
+BEASTS=31
+BROUGHT=15
+BRUTISH=31
+BRUTUS=4, 8, 9, 13, 14, 20, 21, 25, 27
+BURY=2
+BUT=13, 28
+CAESAR=2, 4, 5, 7, 11, 17
+CAPTIVES=15
+CAUSE=29, 30
+COESAR=18
+COFFERS=16
+COME=2, 11
+COUNTRYMEN=1
+CRIED=18
+CROWN=23
+D=7
+DID=16, 17, 22, 24, 29
+DISPROVE=27
+DO=28
+EARS=1
+FAITHFUL=12
+FAULT=6
+FELD=31
+FILL=16
+FOR=9, 30
+FRIEND=12
+FRIENDS=1
+FUNERAL=11
+GENERAL=16
+GOOD=3
+.
+.
+.
+.
+
+*/
+
+//OUTPUT using TreeMap alphabetically ordered output
+/*
+STERNER=19
+SPEAK=11, 27, 28
+COESAR=18
+POOR=18
+LOST=32
+AFTER=3
+HATH=5, 7, 15, 18
+SHOULD=19
+MOURN=30
+THEY=10
+CRIED=18
+WHEN=18
+BRUTISH=31
+TOLD=5
+WHICH=24
+JUDGEMENT=31
+YOUR=1
+COME=2, 11
+FUNERAL=11
+IF=6
+GRIEVOUSLY=7
+IN=11
+IS=3, 9, 14, 17, 21, 26
+IT=4, 6, 6, 7
+AM=28
+AN=9, 14, 26
+SEE=22
+SURE=26
+ARE=10
+MADE=19
+THRICE=23, 24
+WITHOUT=29
+THEN=30
+THEM=3
+MAN=9, 14, 21, 26
+AMBITION=19, 24
+KINGLY=23
+A=6, 23
+CROWN=23
+BE=4, 19
+ART=31
+D=7
+*/
